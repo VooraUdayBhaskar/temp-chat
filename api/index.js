8ca327a2847bd16d02a6e1d71d6a51da943c0cc3
Uday Bhaskar Voora <udayvoora@gmail.com>
2025-09-14 10:17:43 -0700

    cjhange to requiremtns file

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ const AUTH0_CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET;
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=" + GEMINI_API_KEY;
 
+// --- Force a new token request for debugging ---
 let auth0Token = '';
 let tokenExpiry = 0;
 
